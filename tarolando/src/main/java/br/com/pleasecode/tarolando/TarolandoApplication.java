ff6fb0c3ea7e831652a80f524a256640bb5b5b14
Fernando <Fernando@DESKTOP-055NM50>
2018-08-22 21:27:17 -0300

    primeiro commit

---
+++ tarolando/src/main/java/br/com/pleasecode/tarolando/TarolandoApplication.java
@@ -0,0 +1,12 @@
+package br.com.pleasecode.tarolando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarolandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarolandoApplication.class, args);
+	}
+}
