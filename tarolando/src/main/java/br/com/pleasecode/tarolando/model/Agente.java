59b92a0e07dc7800cdc1c08cf54edc8d003994a0
Fernando <Fernando@DESKTOP-055NM50>
2018-08-22 22:36:57 -0300

    adiciona spring data jpa

--- tarolando/src/main/java/br/com/pleasecode/tarolando/model/Agente.java
+++ tarolando/src/main/java/br/com/pleasecode/tarolando/model/Agente.java
@@ -1,5 +1,23 @@
 package br.com.pleasecode.tarolando.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Agente {
+	private int codAgente;
+	private String nome;
+	private String email;
+	private String sexo;
+	private String cpf;
+	private String rg;
+	private String nomeMae;
+	private boolean ehCliente;
+	private boolean ehFuncionario;
+	private boolean ehIndicador;
+	private String documentoFoto;
+	private String documentoTipo;
+	private String imagemUrl;	
+	private int codPlano;
+	private boolean ativo;
 
 }
