8e0b8f007d9884b5d126aba53c858ca05e3e1e2e
Fernando <Fernando@DESKTOP-055NM50>
2018-08-24 01:56:30 -0300

    # ajusta controllers

--- tarolando/src/main/java/br/com/pleasecode/tarolando/model/Configuracao.java
+++ tarolando/src/main/java/br/com/pleasecode/tarolando/model/Configuracao.java
@@ -3,9 +3,11 @@ package br.com.pleasecode.tarolando.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@Entity
 public class Configuracao extends AbstractEntity  {
 
 	@Column(name="MOMENTO")
